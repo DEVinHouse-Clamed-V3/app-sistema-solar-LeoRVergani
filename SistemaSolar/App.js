f6d804cb9f3d92fe6b53258ab6b8ef390b84299a
leorvergani <vergani@outlook.com.br>
2024-09-16 23:24:08 -0300

    Finalizado o Projeto dos Planetas

--- SistemaSolar/App.js
+++ SistemaSolar/App.js
@@ -1,20 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, SafeAreaView, StyleSheet, Image, StatusBar, ScrollView, TouchableOpacity, ImageBackground,
+} from "react-native";
+
+import { useState } from "react";
+import { Planeta } from "./Planeta";
+import { planetas } from "./planetas";
 
 export default function App() {
+  const [indicePlaneta, atualizarIndice] = useState(0);
+
+  function anteriorBtn() {
+    if (indicePlaneta >= 1) {
+      atualizarIndice(indicePlaneta - 1);
+    }
+  }
+
+  function proximoBtn() {
+    if (indicePlaneta <= planetas.length - 2) {
+      atualizarIndice(indicePlaneta + 1);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="#000" />
+      <ImageBackground
+        source={require("./assets/background.webp")}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%", flex: 1 }}
+      >
+        <ScrollView>
+        <View style={styles.header}>
+          <Text
+            style={{
+              fontSize: 22,
+              color: "#fff",
+              fontWeight: "bold",
+              textTransform: "uppercase",
+            }}
+          >
+            Vamos Explorar
+          </Text>
+          <Image style={styles.logo} source={require("./assets/logo.png")} />
+        </View>
+
+        
+          <Planeta planeta={planetas[indicePlaneta]}></Planeta>
+
+          <View style={styles.buttons}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => anteriorBtn()}
+            >
+              <Text style={{ fontSize: 14, color: "#fff", fontWeight: "bold" }}>Anterior</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() =>  proximoBtn()}
+            >
+              <Text style={{ fontSize: 14, color: "#fff", fontWeight: "bold" }}>Próximo</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    flexDirection: "row",
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 70,
+    height: 70,
+    marginLeft: 20
+  },
+  buttons: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 30,
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#000",
+    borderRadius: 15,
+    padding: 15,
+  }
 });
